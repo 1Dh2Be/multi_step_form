fc0563df0c16c6b903fec294b87a381da5bacc31
Mimoun <mimounb1597@gmail.com>
2024-12-16 19:14:34 +0100

    step 2 styling

---
+++ src/pages/components/toggler/Toggler.jsx
@@ -0,0 +1,12 @@
+import "./Toggler.css"
+
+const Toggler = () => {
+    return (
+        <>
+            <input type="checkbox" id="billing__toggler" className="billing__input"/>
+            <span className="toggle__slider"></span>
+        </>
+    )
+}
+
+export default Toggler
--- src/pages/form-step2/FormStep2.jsx
+++ src/pages/form-step2/FormStep2.jsx
@@ -1,6 +1,10 @@
+import "./FormStep2.css"
+
+
 import { useNavigate } from "react-router";
 import { useForm } from "../FormContext";
 import FormTemplate from "../components/form/FormTemplate";
+import Toggler from "../components/toggler/Toggler";
 
 
 const FormStep2 = () => {
@@ -27,6 +31,48 @@ const FormStep2 = () => {
             onBackStep={handleBackStep}
             showBackButton= {true}
         >
+          <div className="plans">
+            <label className="plan">
+              <input type="radio" name="plan" value="arcade" defaultChecked />
+              <div className="plan__content">
+                <span className="icon"><img src="src/assets/images/icon-arcade.svg" alt="Arcade plan icon" /></span>
+                <div>
+                  <h3>Arcade</h3>
+                  <p>$9/mo</p>
+                </div>
+              </div>
+            </label>
+
+            <label className="plan">
+              <input type="radio" name="plan" value="advanced" />
+              <div className="plan__content">
+                <span className="icon"><img src="src/assets/images/icon-advanced.svg" alt="Advanced plan icon" /></span>
+                <div>
+                  <h3>Advanced</h3>
+                  <p>$12/mo</p>
+                </div>
+              </div>
+            </label>
+
+            <label className="plan">
+              <input type="radio" name="plan" value="pro" />
+              <div className="plan__content">
+                <span className="icon"><img src="src/assets/images/icon-pro.svg" alt="Pro plan icon" /></span>
+                <div>
+                  <h3>Pro</h3>
+                  <p>$15/mo</p>
+                </div>
+              </div>
+            </label>
+          </div>
+
+          <div className="billing__toggler">
+            <label className="billing__label">
+              Monthly
+              <Toggler/>
+              yearly
+            </label>
+          </div>
 
         </FormTemplate>
     )
