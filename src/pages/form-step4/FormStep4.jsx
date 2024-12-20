a849eda0fb90a51fefbaff26a452857afc25139c
Mimoun <mimounb1597@gmail.com>
2024-12-20 20:48:31 +0100

    Add dynamic pricing and total price calculation for plans and add-ons

--- src/pages/form-step4/FormStep4.jsx
+++ src/pages/form-step4/FormStep4.jsx
@@ -3,10 +3,40 @@ import FormTemplate from "../components/form/FormTemplate"
 import "./FormStep4.css"
 import { useForm } from "../FormContext"
 
+const PLAN_PRICE = {
+    arcade: {
+        monthly: 9,
+        yearly: 90
+    },
+    advanced: {
+        monthly: 12,
+        yearly: 120
+    },
+    pro: {
+        monthly: 15,
+        yearly: 150
+    }
+}
+
+const ADDON_PRICE = {
+    "Online-service": {
+        monthly: 1,
+        yearly: 10
+    },
+    "Extra-storage": {
+        monthly: 2,
+        yearly: 20
+    },
+    "Custom-profile": {
+        monthly: 2,
+        yearly: 20
+    }
+}
+
 
 const FormStep4 = () => {
 
-    const {currentStep, setCurrentStep, formData, resetForm} = useForm();
+    const {currentStep, setCurrentStep, isChecked, formData, resetForm} = useForm();
     const navigate = useNavigate();
 
     const handleBackStep = () => {
@@ -18,6 +48,17 @@ const FormStep4 = () => {
         resetForm();
         navigate("/step1")
     }
+
+    const planPrice = PLAN_PRICE[formData.step2.plan][formData.step2.billingCycle];
+
+    const addonsPrice = Object.keys(formData.step3.addons).reduce((total, key) => {
+        if (formData.step3.addons[key] === 1) {
+            total += ADDON_PRICE[key][formData.step2.billingCycle];
+        }
+        return total;
+    }, 0);
+
+    const totalPrice = planPrice + addonsPrice;
 
     return (
         <FormTemplate
@@ -36,7 +77,7 @@ const FormStep4 = () => {
                                 <h3 className="current__plan--title">{`${formData.step2.plan} (${formData.step2.billingCycle})`}</h3>
                                 <a href="step2" onClick={() => setCurrentStep(2)} className="change__plan" aria-label="Change plan">Change</a>
                             </div>
-                            <span className="curremt__plan--price">$9/mo</span>
+                            <span className="curremt__plan--price">{`$${planPrice}/${isChecked? "yr" : "mo"}`}</span>
                         </section>
 
                         <span className="divider"></span>
@@ -47,7 +88,7 @@ const FormStep4 = () => {
                                     formData.step3.addons[key] === 1 && (
                                         <li className="addon__list" key={key}>
                                             <h3 className="addon__name">{key.replace(/-/g, " ")}</h3>
-                                            <span className="addon__price">+$1/mo</span>
+                                            <span className="addon__price">{`+$${ADDON_PRICE[key][formData.step2.billingCycle]}/${isChecked? "yr" : "mo"}`}</span>
                                         </li>
                                     )
                                 )}
@@ -58,7 +99,7 @@ const FormStep4 = () => {
 
             <section className="plan__total">
                 <h3 className="plan__total--title">{`Total (per ${formData.step2.billingCycle.slice(0, -2)})`}</h3>
-                <span className="plan__total--price">$12/mo</span>
+                <span className="plan__total--price">{`$${totalPrice}/${isChecked? "yr" : "mo"}`}</span>
             </section>
         </FormTemplate>
     )
